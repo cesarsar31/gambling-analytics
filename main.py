ac804a0ac45df3f422d27e829828c57d0359c538
cesarsar31 <cesarsar31@gmail.com>
2019-04-22 14:22:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# [START gae_python37_render_template]
 import datetime
 
 from flask import Flask, render_template, flash, redirect, request, url_for
@@ -26,7 +12,7 @@ app = Flask(__name__)
 def index():
     
 
-    return render_template('index.html', data=[{'name':'Match Stats'}, {'name':'Attack'}, {'name':'Defense'}, {'name':'Match Odds'}])
+    return render_template('index.html', data=[{'name':'Ver partidos del día'}, {'name':'Ver partidos En Juego'}])
 
 
 @app.route('/result', methods=['GET', 'POST'])
